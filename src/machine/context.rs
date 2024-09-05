694b51a4ed44fe10c2202a9a2fcdbf21274dbd7d
rvcas <x@rvcas.dev>
2024-09-05 14:01:52 -0400

    feat: impl apply to a lambda

--- src/machine/context.rs
+++ src/machine/context.rs
@@ -24,4 +24,53 @@ impl<'a> Context<'a> {
     pub fn no_frame(arena: &'a Bump) -> &'a Context<'a> {
         arena.alloc(Context::NoFrame)
     }
+
+    pub fn frame_await_arg(
+        arena: &'a Bump,
+        function: &'a Value<'a>,
+        context: &'a Context<'a>,
+    ) -> &'a Context<'a> {
+        arena.alloc(Context::FrameAwaitArg(function, context))
+    }
+
+    pub fn frame_await_fun_term(
+        arena: &'a Bump,
+        arg_env: &'a Env<'a>,
+        argument: &'a Term<'a>,
+        context: &'a Context<'a>,
+    ) -> &'a Context<'a> {
+        arena.alloc(Context::FrameAwaitFunTerm(arg_env, argument, context))
+    }
+
+    pub fn frame_await_fun_value(
+        arena: &'a Bump,
+        function: &'a Value<'a>,
+        context: &'a Context<'a>,
+    ) -> &'a Context<'a> {
+        arena.alloc(Context::FrameAwaitFunValue(function, context))
+    }
+
+    pub fn frame_force(arena: &'a Bump, context: &'a Context<'a>) -> &'a Context<'a> {
+        arena.alloc(Context::FrameForce(context))
+    }
+
+    pub fn frame_constr(
+        arena: &'a Bump,
+        env: &'a Env<'a>,
+        index: usize,
+        terms: BumpVec<'a, Term<'a>>,
+        values: BumpVec<'a, Value<'a>>,
+        context: &'a Context<'a>,
+    ) -> &'a Context<'a> {
+        arena.alloc(Context::FrameConstr(env, index, terms, values, context))
+    }
+
+    pub fn frame_cases(
+        arena: &'a Bump,
+        env: &'a Env<'a>,
+        terms: BumpVec<'a, Term<'a>>,
+        context: &'a Context<'a>,
+    ) -> &'a Context<'a> {
+        arena.alloc(Context::FrameCases(env, terms, context))
+    }
 }
--- src/machine/env.rs
+++ src/machine/env.rs
@@ -3,14 +3,22 @@ use bumpalo::{collections::Vec as BumpVec, Bump};
 use super::value::Value;
 
 #[derive(Debug)]
-pub struct Env<'a>(BumpVec<'a, Value<'a>>);
+pub struct Env<'a>(BumpVec<'a, &'a Value<'a>>);
 
 impl<'a> Env<'a> {
     pub fn new_in(arena: &'a Bump) -> &'a Self {
         arena.alloc(Self(BumpVec::new_in(arena)))
     }
 
-    pub fn lookup(&'a self, name: usize) -> Option<&'a Value<'a>> {
+    pub fn push(&'a self, arena: &'a Bump, argument: &'a Value<'a>) -> &'a mut Self {
+        let mut new_env = self.0.clone();
+
+        new_env.push(argument);
+
+        arena.alloc(Self(new_env))
+    }
+
+    pub fn lookup(&'a self, name: usize) -> Option<&&'a Value<'a>> {
         self.0.get(self.0.len() - name)
     }
 }
