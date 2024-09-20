b903055fb348b09db667409e0c7f868d830ce995
rvcas <x@rvcas.dev>
2024-09-20 12:49:42 -0400

    feat: add data parsing

--- crates/uplc/src/syn/data.rs
+++ crates/uplc/src/syn/data.rs
@@ -15,6 +15,8 @@ pub fn parser<'a>() -> impl Parser<'a, &'a str, &'a PlutusData<'a>, Extra<'a>> {
                 .padded()
                 .ignore_then(just('#').ignore_then(hex_bytes()).padded())
                 .map_with(|v, e: &mut MapExtra<'a, '_>| {
+                    dbg!("b", &v);
+
                     let state = e.state();
 
                     let bytes = BumpVec::from_iter_in(v, state.arena);
@@ -23,27 +25,82 @@ pub fn parser<'a>() -> impl Parser<'a, &'a str, &'a PlutusData<'a>, Extra<'a>> {
                 }),
             just('I')
                 .padded()
-                .ignore_then(text::int(10).padded())
+                .ignore_then(
+                    text::int(10)
+                        .or(just('-').padded().ignore_then(text::int(10)))
+                        .padded(),
+                )
                 .map_with(|v, e: &mut MapExtra<'a, '_>| {
+                    dbg!("i", &v);
+
                     let state = e.state();
 
-                    PlutusData::integer_from(state.arena, v.parse().unwrap())
+                    let value = if let Some(v) = v.strip_prefix('-') {
+                        -(v.parse::<i128>().unwrap())
+                    } else {
+                        v.parse::<i128>().unwrap()
+                    };
+
+                    PlutusData::integer_from(state.arena, value)
                 }),
             just("Constr")
                 .padded()
                 .ignore_then(text::int(10).padded())
                 .then(
-                    data.separated_by(just(',').padded())
+                    data.clone()
+                        .separated_by(just(',').padded())
                         .collect()
                         .delimited_by(just('['), just(']')),
                 )
                 .map_with(
                     |(tag, fields): (_, Vec<&PlutusData<'_>>), e: &mut MapExtra<'a, '_>| {
+                        dbg!("c", &tag, &fields);
+
                         let state = e.state();
 
                         let fields = BumpVec::from_iter_in(fields, state.arena);
 
                         PlutusData::constr(state.arena, tag.parse().unwrap(), fields)
+                    },
+                ),
+            just("List")
+                .padded()
+                .ignore_then(
+                    data.clone()
+                        .separated_by(just(',').padded())
+                        .collect()
+                        .delimited_by(just('['), just(']')),
+                )
+                .map_with(|items: Vec<&PlutusData<'_>>, e: &mut MapExtra<'a, '_>| {
+                    dbg!("c", &items);
+
+                    let state = e.state();
+
+                    let fields = BumpVec::from_iter_in(items, state.arena);
+
+                    PlutusData::list(state.arena, fields)
+                }),
+            just("Map")
+                .padded()
+                .ignore_then(
+                    data.clone()
+                        .padded()
+                        .then_ignore(just(',').padded())
+                        .then(data.padded())
+                        .delimited_by(just('('), just(')'))
+                        .separated_by(just(',').padded())
+                        .collect()
+                        .padded()
+                        .delimited_by(just('['), just(']'))
+                        .padded(),
+                )
+                .map_with(
+                    |items: Vec<(&PlutusData<'_>, &PlutusData<'_>)>, e: &mut MapExtra<'a, '_>| {
+                        let state = e.state();
+
+                        let fields = BumpVec::from_iter_in(items, state.arena);
+
+                        PlutusData::map(state.arena, fields)
                     },
                 ),
         ))
