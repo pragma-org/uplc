337f21662435cdeea450812a037dc89f6d4e2ab1
rvcas <x@rvcas.dev>
2024-10-12 01:10:06 -0400

    feat: add some crypto functions

--- crates/uplc/src/machine/error.rs
+++ crates/uplc/src/machine/error.rs
@@ -11,35 +11,60 @@ use crate::{
 
 use super::{value::Value, ExBudget};
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum MachineError<'a> {
+    #[error("Explicit error term")]
     ExplicitErrorTerm,
+    #[error("Non-function application")]
     NonFunctionApplication(&'a Value<'a>, &'a Value<'a>),
+    #[error("Non-constant value")]
     NotAConstant(&'a Value<'a>),
+    #[error("Open term evaluated")]
     OpenTermEvaluated(&'a Term<'a>),
+    #[error("Out of budget")]
     OutOfExError(ExBudget),
+    #[error("Unexpected builtin term argument")]
     UnexpectedBuiltinTermArgument(&'a Term<'a>),
+    #[error("Non-polymorphic instantiation")]
     NonPolymorphicInstantiation(&'a Value<'a>),
+    #[error("Builtin term argument expected")]
     BuiltinTermArgumentExpected(&'a Term<'a>),
+    #[error("Non-constructor scrutinized")]
     NonConstrScrutinized(&'a Value<'a>),
+    #[error("Non-integer index")]
     MissingCaseBranch(&'a [&'a Term<'a>], &'a Value<'a>),
+    #[error(transparent)]
     Runtime(RuntimeError<'a>),
 }
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum RuntimeError<'a> {
+    #[error("Byte string out of bounds")]
     ByteStringOutOfBounds(&'a BumpVec<'a, u8>, &'a Integer),
+    #[error("Type mismatch")]
     TypeMismatch(Type<'a>, &'a Constant<'a>),
+    #[error("Expected pair")]
     ExpectedPair(&'a Constant<'a>),
+    #[error("Expected list")]
     ExpectedList(&'a Constant<'a>),
+    #[error("Not data")]
     NotData(&'a Constant<'a>),
+    #[error("Malformed data")]
     MalFormedData(&'a PlutusData<'a>),
+    #[error("Empty list")]
     EmptyList(&'a BumpVec<'a, &'a Constant<'a>>),
+    #[error("Unexpected Ed25519 public key length")]
     UnexpectedEd25519PublicKeyLength(TryFromSliceError),
+    #[error("Unexpected Ed25519 signature length")]
     UnexpectedEd25519SignatureLength(TryFromSliceError),
+    #[error("Division by zero")]
     DivisionByZero(&'a Integer, &'a Integer),
+    #[error("MkCons type mismatch")]
     MkConsTypeMismatch(&'a Constant<'a>),
+    #[error("Byte string cons not a byte")]
     ByteStringConsNotAByte(&'a Integer),
+    #[error(transparent)]
+    Secp256k1(#[from] secp256k1::Error),
 }
 
 impl<'a> MachineError<'a> {
@@ -93,5 +118,9 @@ impl<'a> MachineError<'a> {
 
     pub fn byte_string_cons_not_a_byte(byte: &'a Integer) -> Self {
         MachineError::runtime(RuntimeError::ByteStringConsNotAByte(byte))
+    }
+
+    pub fn secp256k1(error: secp256k1::Error) -> Self {
+        MachineError::runtime(RuntimeError::Secp256k1(error))
     }
 }
