716b76c5e29de3056e07825d4c42a09e4b37507e
rvcas <x@rvcas.dev>
2024-12-31 13:55:10 -0500

    feat: constant encoding and plutus data to cbor encoding

--- crates/uplc/src/flat/encode/error.rs
+++ crates/uplc/src/flat/encode/error.rs
@@ -1,3 +1,5 @@
+use std::convert::Infallible;
+
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -6,4 +8,8 @@ pub enum FlatEncodeError {
     Overflow { byte: u8, num_bits: usize },
     #[error("Buffer is not byte aligned")]
     BufferNotByteAligned,
+    #[error("Cannot encode BLS12-381 constants")]
+    BlsElementNotSupported,
+    #[error(transparent)]
+    EncodeCbor(#[from] minicbor::encode::Error<Infallible>),
 }
--- crates/uplc/src/flat/tag.rs
+++ crates/uplc/src/flat/tag.rs
@@ -14,3 +14,16 @@ pub const ERROR: u8 = 6;
 pub const BUILTIN: u8 = 7;
 pub const CONSTR: u8 = 8;
 pub const CASE: u8 = 9;
+
+// Constant Tags
+pub const INTEGER: u8 = 0;
+pub const BYTE_STRING: u8 = 1;
+pub const STRING: u8 = 2;
+pub const UNIT: u8 = 3;
+pub const BOOL: u8 = 4;
+pub const DATA: u8 = 8;
+pub const PROTO_LIST_ONE: u8 = 7;
+pub const PROTO_LIST_TWO: u8 = 5;
+pub const PROTO_PAIR_ONE: u8 = 7;
+pub const PROTO_PAIR_TWO: u8 = 7;
+pub const PROTO_PAIR_THREE: u8 = 6;
