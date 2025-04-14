fb796fde7b0c7ccee3ad43e1b90dc97bae813b3f
rvcas <x@rvcas.dev>
2025-04-14 12:15:24 -0400

    chore: more name fixes

--- crates/uplc/tests/conformance.rs
+++ crates/uplc/tests/conformance.rs
@@ -3,7 +3,7 @@ use uplc_macros::generate_tests;
 fn run_test(file_contents: &str, expected_output: &str, expected_budget: &str) {
     let arena = bumpalo::Bump::new();
 
-    let Ok(program) = uplc::syn::parse_program(&arena, file_contents).into_result() else {
+    let Ok(program) = uplc_turbo::syn::parse_program(&arena, file_contents).into_result() else {
         pretty_assertions::assert_eq!("parse error", expected_output);
         pretty_assertions::assert_eq!("parse error", expected_budget);
 
@@ -21,7 +21,7 @@ fn run_test(file_contents: &str, expected_output: &str, expected_budget: &str) {
         return;
     };
 
-    let expected = uplc::syn::parse_program(&arena, expected_output)
+    let expected = uplc_turbo::syn::parse_program(&arena, expected_output)
         .into_result()
         .unwrap();
 
