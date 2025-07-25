4f22475bc8308cf8eaf2a9ee6d580ee4eeae902f
yHSJ <josh@securitybot.info>
2025-07-25 18:26:10 -0400

    fix: parse names correctly
    Signed-off-by: yHSJ <josh@securitybot.info>

--- crates/uplc/src/syn/utils.rs
+++ crates/uplc/src/syn/utils.rs
@@ -2,6 +2,17 @@ use chumsky::prelude::*;
 
 use super::types::Extra;
 
+pub fn name<'a>() -> impl Parser<'a, &'a str, &'a str, Extra<'a>> {
+    any()
+        .filter(|c: &char|  c.is_ascii_alphabetic())
+        .then(
+            select! {
+                c if (c as char).is_ascii_alphanumeric() || (c as char) == '_' || (c as char) == '-' || (c as char) == '\'' => ()
+            }.repeated()
+        )
+        .to_slice()
+}
+
 pub fn hex_digit<'a>() -> impl Parser<'a, &'a str, u8, Extra<'a>> {
     one_of("0123456789abcdefABCDEF").map(|c: char| c.to_digit(16).unwrap() as u8)
 }
