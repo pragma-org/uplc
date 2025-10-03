cb3387b6c9e130e19de6a12f5e4fe4e8ff0c9be1
Jonathan Lim <jonathan.lim.222@gmail.com>
2025-10-03 11:46:34 -0500

    feat: add builtin cost model B
    Signed-off-by: Jonathan Lim <jonathan.lim.222@gmail.com>

--- crates/uplc/src/machine/cost_model/builtin_costs.rs
+++ crates/uplc/src/machine/cost_model/builtin_costs.rs
@@ -1087,6 +1087,384 @@ impl BuiltinCosts {
         }
     }
 
+    pub fn v2() -> Self {
+        Self {
+            add_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::max_size(1, 1),
+                TwoArgumentsCosting::max_size(100788, 420),
+            ),
+            subtract_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::max_size(1, 1),
+                TwoArgumentsCosting::max_size(100788, 420),
+            ),
+            multiply_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::added_sizes(0, 1),
+                TwoArgumentsCosting::multiplied_sizes(90434, 519),
+            ),
+            divide_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::subtracted_sizes(0, 1, 1),
+                TwoArgumentsCosting::const_above_diagonal_into_multiplied_sizes(85848, 228465, 122),
+            ),
+            quotient_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::subtracted_sizes(0, 1, 1),
+                TwoArgumentsCosting::const_above_diagonal_into_multiplied_sizes(
+                    85848, 228465, 122
+                ),
+            ),
+            remainder_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::subtracted_sizes(0, 1, 1),
+                TwoArgumentsCosting::const_above_diagonal_into_multiplied_sizes(
+                    85848, 228465, 122,
+                ),
+            ),
+            mod_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::subtracted_sizes(0, 1, 1),
+                TwoArgumentsCosting::const_above_diagonal_into_multiplied_sizes(85848, 228465, 122),
+            ),
+            equals_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::min_size(51775, 558),
+            ),
+            less_than_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::min_size(44749, 541),
+            ),
+            less_than_equals_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::min_size(43285, 552),
+            ),
+            append_byte_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::added_sizes(0, 1),
+                TwoArgumentsCosting::added_sizes(1000, 173),
+            ),
+            cons_byte_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::added_sizes(0, 1),
+                TwoArgumentsCosting::linear_in_y(72010, 178),
+            ),
+            slice_byte_string: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::linear_in_z(4, 0),
+                ThreeArgumentsCosting::linear_in_z(20467, 1),
+            ),
+            length_of_byte_string: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(10),
+                OneArgumentCosting::constant_cost(22100),
+            ),
+            index_byte_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(4),
+                TwoArgumentsCosting::constant_cost(13169),
+            ),
+            equals_byte_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::linear_on_diagonal(24548, 29498, 38),
+            ),
+            less_than_byte_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::min_size(28999, 74),
+            ),
+            less_than_equals_byte_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::min_size(28999, 74),
+            ),
+            sha2_256: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(4),
+                OneArgumentCosting::linear_cost(270652, 22588),
+            ),
+            sha3_256: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(4),
+                OneArgumentCosting::linear_cost(1457325, 64566),
+            ),
+            blake2b_256: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(4),
+                OneArgumentCosting::linear_cost(201305, 8356),
+            ),
+            verify_ed25519_signature: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::constant_cost(10),
+                ThreeArgumentsCosting::linear_in_y(53384111, 14333),
+            ),
+            verify_ecdsa_secp256k1_signature: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::constant_cost(10),
+                ThreeArgumentsCosting::constant_cost(43053543),
+            ),
+            verify_schnorr_secp256k1_signature: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::constant_cost(10),
+                ThreeArgumentsCosting::linear_in_y(43574283, 26308),
+            ),
+            append_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::added_sizes(4, 1),
+                TwoArgumentsCosting::added_sizes(1000, 59957),
+            ),
+            equals_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::linear_on_diagonal(39184, 1000, 60594),
+            ),
+            encode_utf8: OneArgumentCosting::new(
+                OneArgumentCosting::linear_cost(4, 2),
+                OneArgumentCosting::linear_cost(1000, 42921),
+            ),
+            decode_utf8: OneArgumentCosting::new(
+                OneArgumentCosting::linear_cost(4, 2),
+                OneArgumentCosting::linear_cost(91189, 769),
+            ),
+            if_then_else: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::constant_cost(1),
+                ThreeArgumentsCosting::constant_cost(76049),
+            ),
+            choose_unit: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(4),
+                TwoArgumentsCosting::constant_cost(61462),
+            ),
+            trace: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(32),
+                TwoArgumentsCosting::constant_cost(59498),
+            ),
+            fst_pair: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(141895),
+            ),
+            snd_pair: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(141992),
+            ),
+            choose_list: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::constant_cost(32),
+                ThreeArgumentsCosting::constant_cost(132994),
+            ),
+            mk_cons: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(32),
+                TwoArgumentsCosting::constant_cost(72362),
+            ),
+            head_list: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(83150),
+            ),
+            tail_list: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(81663),
+            ),
+            null_list: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(74433),
+            ),
+            choose_data: SixArgumentsCosting::new(
+                SixArgumentsCosting::constant_cost(32),
+                SixArgumentsCosting::constant_cost(94375),
+            ),
+            constr_data: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(32),
+                TwoArgumentsCosting::constant_cost(22151),
+            ),
+            map_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(68246),
+            ),
+            list_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(33852),
+            ),
+            i_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(15299),
+            ),
+            b_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(11183),
+            ),
+            un_constr_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(24588),
+            ),
+            un_map_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(24623),
+            ),
+            un_list_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(25933),
+            ),
+            un_i_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(20744),
+            ),
+            un_b_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(20142),
+            ),
+            equals_data: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::min_size(898148, 27279),
+            ),
+            mk_pair_data: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(32),
+                TwoArgumentsCosting::constant_cost(11546),
+            ),
+            mk_nil_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(7243),
+            ),
+            mk_nil_pair_data: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(32),
+                OneArgumentCosting::constant_cost(7391),
+            ),
+
+            serialise_data: OneArgumentCosting::new(
+                OneArgumentCosting::linear_cost(0, 2),
+                OneArgumentCosting::linear_cost(955506, 213312),
+            ),
+            blake2b_224: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(4),
+                OneArgumentCosting::linear_cost(207616, 8310),
+            ),
+            keccak_256: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(4),
+                OneArgumentCosting::linear_cost(2261318, 64571),
+            ),
+            bls12_381_g1_add: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(18),
+                TwoArgumentsCosting::constant_cost(962335),
+            ),
+            bls12_381_g1_neg: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(18),
+                OneArgumentCosting::constant_cost(267929),
+            ),
+            bls12_381_g1_scalar_mul: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(18),
+                TwoArgumentsCosting::linear_in_x(76433006, 8868),
+            ),
+            bls12_381_g1_equal: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::constant_cost(442008),
+            ),
+            bls12_381_g1_compress: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(6),
+                OneArgumentCosting::constant_cost(2780678),
+            ),
+            bls12_381_g1_uncompress: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(18),
+                OneArgumentCosting::constant_cost(52948122),
+            ),
+            bls12_381_g1_hash_to_group: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(18),
+                TwoArgumentsCosting::linear_in_x(52538055, 3756),
+            ),
+            bls12_381_g2_add: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(36),
+                TwoArgumentsCosting::constant_cost(1995836),
+            ),
+            bls12_381_g2_neg: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(36),
+                OneArgumentCosting::constant_cost(284546),
+            ),
+            bls12_381_g2_scalar_mul: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(36),
+                TwoArgumentsCosting::linear_in_x(158_221_314, 26_549),
+            ),
+            bls12_381_g2_equal: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::constant_cost(901_022),
+            ),
+            bls12_381_g2_compress: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(12),
+                OneArgumentCosting::constant_cost(3_227_919),
+            ),
+            bls12_381_g2_uncompress: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(36),
+                OneArgumentCosting::constant_cost(74_698_472),
+            ),
+            bls12_381_g2_hash_to_group: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(36),
+                TwoArgumentsCosting::linear_in_x(166_917_843, 4_307),
+            ),
+            bls12_381_miller_loop: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(72),
+                TwoArgumentsCosting::constant_cost(254006273),
+            ),
+            bls12_381_mul_ml_result: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(72),
+                TwoArgumentsCosting::constant_cost(2174038),
+            ),
+            bls12_381_final_verify: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::constant_cost(333849714),
+            ),
+            integer_to_byte_string: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::literal_in_y_or_linear_in_z(0, 1),
+                ThreeArgumentsCosting::quadratic_in_z(1293828, 28716, 63),
+            ),
+            byte_string_to_integer: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::linear_in_y(0, 1),
+                TwoArgumentsCosting::quadratic_in_y(1006041, 43623, 251),
+            ),
+            and_byte_string: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::linear_in_max_y_z(0, 1),
+                ThreeArgumentsCosting::linear_in_y_and_z(100181, 726, 719),
+            ),
+            or_byte_string: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::linear_in_max_y_z(0, 1),
+                ThreeArgumentsCosting::linear_in_y_and_z(100181, 726, 719),
+            ),
+            xor_byte_string: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::linear_in_max_y_z(0, 1),
+                ThreeArgumentsCosting::linear_in_y_and_z(100181, 726, 719),
+            ),
+            complement_byte_string: OneArgumentCosting::new(
+                OneArgumentCosting::linear_cost(0, 1),
+                OneArgumentCosting::linear_cost(107878, 680),
+            ),
+            read_bit: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(1),
+                TwoArgumentsCosting::constant_cost(95336),
+            ),
+            write_bits: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::linear_in_x(0, 1),
+                ThreeArgumentsCosting::linear_in_y(281145, 18848),
+            ),
+            replicate_byte: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::linear_in_x(1, 1),
+                TwoArgumentsCosting::linear_in_x(180194, 159),
+            ),
+            shift_byte_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::linear_in_x(0, 1),
+                TwoArgumentsCosting::linear_in_x(158519, 8942),
+            ),
+            rotate_byte_string: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::linear_in_x(0, 1),
+                TwoArgumentsCosting::linear_in_x(159378, 8813),
+            ),
+            count_set_bits: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(1),
+                OneArgumentCosting::linear_cost(107490, 3298),
+            ),
+            find_first_set_bit: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(1),
+                OneArgumentCosting::linear_cost(106057, 655),
+            ),
+            ripemd_160: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(3),
+                OneArgumentCosting::linear_cost(1964219, 24520),
+            ),
+            exp_mod_integer: ThreeArgumentsCosting::new(
+                ThreeArgumentsCosting::linear_in_z(0, 1),
+                ThreeArgumentsCosting::exp_mod_cost(607153, 231697, 53144),
+            ),
+            drop_list: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(4),
+                TwoArgumentsCosting::linear_in_x(116711, 1957),
+            ),
+            length_of_array: OneArgumentCosting::new(
+                OneArgumentCosting::constant_cost(10),
+                OneArgumentCosting::constant_cost(198994),
+            ),
+            list_to_array: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::linear_in_x(7, 1),
+                TwoArgumentsCosting::linear_in_x(307802, 8496),
+            ),
+            index_array: TwoArgumentsCosting::new(
+                TwoArgumentsCosting::constant_cost(32),
+                TwoArgumentsCosting::constant_cost(194922),
+            ),
+        }
+    }
+
     pub fn v3() -> Self {
         Self {
             add_integer: TwoArgumentsCosting::new(
