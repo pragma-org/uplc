ebeef9cb5b3ffcba73dcdc248463a3aa25cc6fcc
Jonathan Lim <jonathan.lim.222@gmail.com>
2025-10-08 10:50:19 -0500

    feat: choose cost model based on plutus version
    Signed-off-by: Jonathan Lim <jonathan.lim.222@gmail.com>

--- crates/uplc/src/machine/cost_model/mod.rs
+++ crates/uplc/src/machine/cost_model/mod.rs
@@ -6,10 +6,26 @@ mod value;
 
 pub use value::*;
 
+use crate::machine::PlutusVersion;
+
 #[derive(Default, Debug, PartialEq)]
 pub struct CostModel {
     pub machine_costs: machine_costs::MachineCosts,
     pub builtin_costs: builtin_costs::BuiltinCosts,
+}
+
+impl From<&PlutusVersion> for CostModel {
+    fn from(version: &PlutusVersion) -> Self {
+        let builtin_costs = match version {
+            crate::machine::PlutusVersion::V1 => builtin_costs::BuiltinCosts::v1(),
+            crate::machine::PlutusVersion::V2 => builtin_costs::BuiltinCosts::v2(),
+            crate::machine::PlutusVersion::V3 => builtin_costs::BuiltinCosts::v3(),
+        };
+        Self {
+            machine_costs: Default::default(),
+            builtin_costs,
+        }
+    }
 }
 
 #[repr(usize)]
