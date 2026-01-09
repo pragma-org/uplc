b0112ba8928e73dc06c2c4ff3c78036ecea8e7e0
Jonathan Lim <jonathan.lim.222@gmail.com>
2026-01-09 10:54:02 -0600

    chore: split turbo benchmarks across multiple github jobs
    Signed-off-by: Jonathan Lim <jonathan.lim.222@gmail.com>

--- crates/uplc/benches/bench_main.rs
+++ crates/uplc/benches/bench_main.rs
@@ -3,7 +3,7 @@ use criterion::criterion_main;
 mod benchmarks;
 
 criterion_main! {
-    benchmarks::turbo,
+    benchmarks::turbo::turbo,
     benchmarks::plutus_use_cases,
     benchmarks::add_integer,
     benchmarks::fibonacci,
