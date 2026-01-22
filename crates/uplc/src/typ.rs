0a12c21e96c270d30c81bac83e0991a4c7e41129
Jacob Finkelman <YeomanYaacov@gmail.com>
2026-01-22 16:52:04 -0500

    move to an Arena new type
    Signed-off-by: Jacob Finkelman <YeomanYaacov@gmail.com>
    Signed-off-by: Jonathan Lim <jonathan.lim.222@gmail.com>

--- crates/uplc/src/typ.rs
+++ crates/uplc/src/typ.rs
@@ -1,4 +1,4 @@
-use bumpalo::Bump;
+use crate::arena::Arena;
 
 #[derive(Debug, PartialEq)]
 pub enum Type<'a> {
@@ -17,51 +17,51 @@ pub enum Type<'a> {
 }
 
 impl<'a> Type<'a> {
-    pub fn integer(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn integer(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::Integer)
     }
 
-    pub fn bool(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn bool(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::Bool)
     }
 
-    pub fn string(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn string(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::String)
     }
 
-    pub fn byte_string(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn byte_string(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::ByteString)
     }
 
-    pub fn unit(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn unit(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::Unit)
     }
 
-    pub fn data(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn data(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::Data)
     }
 
-    pub fn list(arena: &'a Bump, inner: &'a Type<'a>) -> &'a Type<'a> {
+    pub fn list(arena: &'a Arena, inner: &'a Type<'a>) -> &'a Type<'a> {
         arena.alloc(Type::List(inner))
     }
 
-    pub fn array(arena: &'a Bump, inner: &'a Type<'a>) -> &'a Type<'a> {
+    pub fn array(arena: &'a Arena, inner: &'a Type<'a>) -> &'a Type<'a> {
         arena.alloc(Type::Array(inner))
     }
 
-    pub fn pair(arena: &'a Bump, fst: &'a Type<'a>, snd: &'a Type<'a>) -> &'a Type<'a> {
+    pub fn pair(arena: &'a Arena, fst: &'a Type<'a>, snd: &'a Type<'a>) -> &'a Type<'a> {
         arena.alloc(Type::Pair(fst, snd))
     }
 
-    pub fn g1(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn g1(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::Bls12_381G1Element)
     }
 
-    pub fn g2(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn g2(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::Bls12_381G2Element)
     }
 
-    pub fn ml_result(arena: &'a Bump) -> &'a Type<'a> {
+    pub fn ml_result(arena: &'a Arena) -> &'a Type<'a> {
         arena.alloc(Type::Bls12_381MlResult)
     }
 }
