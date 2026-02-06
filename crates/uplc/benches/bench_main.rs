ff307660bf6ebb2f1bd627f55a4da5801eb402b2
KtorZ <matthias.benkort@gmail.com>
2026-02-06 10:45:33 +0100

    rework turbo benchmarks split
      - Avoid using macros, and leverage the existing bench filters instead
      - Use one arena per bench function, instead of re-creating one per bench
      - Include the arena's reset as part of the measurement
      - Allow to dynamically configure the number of groups
    
    Closes #35
    
    Signed-off-by: KtorZ <matthias.benkort@gmail.com>
    Signed-off-by: Jonathan Lim <jonathan.lim.222@gmail.com>

--- crates/uplc/benches/bench_main.rs
+++ crates/uplc/benches/bench_main.rs
@@ -3,7 +3,7 @@ use criterion::criterion_main;
 mod benchmarks;
 
 criterion_main! {
-    benchmarks::turbo::turbo,
+    benchmarks::turbo,
     benchmarks::plutus_use_cases,
     benchmarks::add_integer,
     benchmarks::fibonacci,
